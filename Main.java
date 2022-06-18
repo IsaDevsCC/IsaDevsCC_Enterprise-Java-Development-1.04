d4a78c2982e16cfa0b9e599062c9f040bea02c0a
IsaDevsCC <107552354+IsaDevsCC@users.noreply.github.com>
2022-06-18 12:29:59 +0200

    Add files via upload
    Reto laboratorio

---
+++ Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String args[]) {
+
+        RetoApp reto = new RetoApp();
+        reto.diferencia();
+        reto.smallest();
+        reto.fracciones();
+    }
+}
